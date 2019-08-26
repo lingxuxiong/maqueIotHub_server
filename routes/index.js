e154e00d3c688d8115eb5e3b319b93d04ef21f02
Neil Ling <neil.ling@aylanetworks.com>
2019-08-26 22:31:21 +0800

    add some debug info

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('__dirname:' + __dirname)
+  console.log('__filename:' + __filename)
   res.render('index', { title: 'Express' });
 });
+
+router.post('/', function(req, res) {
+  console.log('got post request to /')
+  res.send("ok")
+});
+
+router.put('/user', function(req, res) {
+  console.log('got put request to /user')
+  res.send('ok')
+});
+
 
 module.exports = router;
