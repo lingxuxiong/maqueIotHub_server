164699b3739cfd8b435a9efeeaaae9a93bd24ec3
Neil Ling <neil.ling@aylanetworks.com>
2019-09-14 00:36:40 +0800

    move JWT secret config to dotenv

--- routes/tokens.js
+++ routes/tokens.js
@@ -2,17 +2,12 @@ const jwt = require('jsonwebtoken');
 const shortid = require('shortid');
 const router = require('express').Router();
 
-// secret value must be the same as defined in the jwt plugin config file
-// etc/plugins/emqx_auth_jwt.conf, which looks like
-// auth.jwt.secret = emqxsecret
-const SECRET = 'emqxsecret';
-
 router.post('/', function (_, res) {
     var userName = shortid.generate();
     var password = jwt.sign({
         userName: userName,
         expires: Math.floor(Date.now() / 1000) + 10 * 60
-    }, SECRET);
+    }, process.env.JWT_SECRET);
 
     res.json({
         userName: userName,
